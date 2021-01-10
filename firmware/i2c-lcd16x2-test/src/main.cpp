af4cf8aac1906bd0627fedd3f85e18e427326e3b
Christoph Tack <prog.send@gmail.com>
2021-01-10 14:49:08 +0100

    A little test, writing to a 16x2 character LCD

---
+++ firmware/i2c-lcd16x2-test/src/main.cpp
@@ -0,0 +1,28 @@
+/**
+ * HD44780 LCD16x2 requires 5V (works on 3V3 too, but contrast is very low)
+ * ESP32 is 3V3 logic
+ * => level shifters needed
+ */
+
+//YWROBOT
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(3,0);
+  lcd.print("Hello, world!");
+  lcd.setCursor(2,1);
+  lcd.print("Ywrobot Arduino!");
+}
+
+
+void loop()
+{
+}
