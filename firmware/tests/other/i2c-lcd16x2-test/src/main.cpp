a84d76ccd1dbad069b6272c28a9c2fb9972e61c9
Christoph Tack <prog.send@gmail.com>
2021-08-20 11:47:55 +0200

    Small corrections

--- firmware/tests/other/i2c-lcd16x2-test/src/main.cpp
+++ firmware/tests/other/i2c-lcd16x2-test/src/main.cpp
@@ -1,7 +1,7 @@
 /**
  * HD44780 LCD16x2 requires 5V (works on 3V3 too, but contrast is very low)
  * ESP32 is 3V3 logic
- * => level shifters needed
+ * => level shifters needed for I²C signals
  */
 
 //YWROBOT
@@ -9,16 +9,16 @@
 //Library version:1.1
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
 {
   lcd.init();                      // initialize the lcd 
   // Print a message to the LCD.
   lcd.backlight();
-  lcd.setCursor(3,0);
+  lcd.setCursor(1,0);
   lcd.print("Hello, world!");
-  lcd.setCursor(2,1);
+  lcd.setCursor(0,1);
   lcd.print("Ywrobot Arduino!");
 }
 
